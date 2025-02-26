d9be0ef10732e09d27542861009ac445dfdbf4a5
charliemunro <charlie.munro2@mail.dcu.ie>
2025-02-26 20:03:43 +0000

    Updated MPI programs and added new tests

--- week2/repeat_adder.py
+++ week2/repeat_adder.py
@@ -1,23 +1,24 @@
 import sys
-def main(): 
-	output = 0
 
-	# checks if there are the right number of arguments
-        try:
-		# converts the first two arguments to integers
-		in_arg = int(sys.argv[1])
-                multiple = int(sys.argv[2])
-        except: # (argc != 2)
-		# raises an error
-		raise Exception("Incorrect arguments.\nUsage: python repeat_adder.py [NUM] [NUM]\ne.g.\npython repeat_adder.py 2 3")
-		
-	# iterates over all numbers up to the input
-        for i in range(in_arg):
-		# adds the index to the output
-		output = output + multiple
-	
-	# prints the output
-	print("{}\n".format(output))
+def main():
+    output = 0
+
+    # Checks if there are the right number of arguments
+    try:
+        # Converts the first two arguments to integers
+        in_arg = int(sys.argv[1])
+        multiple = int(sys.argv[2])
+    except:
+        # Raises an error
+        raise Exception("Incorrect arguments.\nUsage: python repeat_adder.py <num> <multiple>")
+
+    # Iterates over all numbers up to the input
+    for i in range(in_arg):
+        # Adds the index to the output
+        output = output + multiple
+
+    # Prints the output
+    print("{}\n".format(output))
 
 if __name__ == "__main__":
-        main()
+    main()
--- week2/time_print.py
+++ week2/time_print.py
@@ -1,29 +1,14 @@
-import sys
-import :time
+import time
 
-def main(): 
-    output = 0
-    # checks if there are the right number of arguments
-    try:
-        # converts the first two arguments to integers
-        in_arg = int(sys.argv[1])
-    except: # (argc != 2)
-        # raises an error
-        raise Exception("Incorrect arguments.\nUsage: python time_print.py [NUM]\ne.g.\npython time_print.py 23")
+def main():
+    start_time = time.time()  # Record start time
 
-    # gets the start time for the loop
-    start_time = time.time()
-    # iterates over all numbers up to the input
-    for i in range(in_arg):
-        # prints the index
-        print("{}, ".format(i), end="")
-    # gets the end time for the loop
-    end_time = time.time()
+    with open("output_python.txt", "w") as file:
+        for i in range(1000000):
+            file.write(f"Line {i}\n")
 
-    # gets the total time
-    run_time = end_time - start_time
-    # prints the output
-    print("\n\nTime for loop: {} seconds\n".format(run_time))
+    end_time = time.time()  # Record end time
+    print(f"Python File Write Time: {end_time - start_time:.6f} seconds")
 
 if __name__ == "__main__":
     main()
